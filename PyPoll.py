2f4dd4e1332d2c14c145d71659adabfa72dee3cc
CBrandt <72421963+cbrandt83@users.noreply.github.com>
2020-12-16 15:06:50 -0800

    assignment code from lesson 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -1,12 +1,19 @@
 #1.Get the data we need to use
-#import csv and os modules
+# Add our dependencies.
 import csv
 import os
-#assign a variable to the file to load and the path
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-#Open and read the file
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
-    print(election_data)
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
 #2. Get the total number of votes cast
 #3. Get a complete list of candidates who received votes
 #4. Get the number of votes each candidate received
