9169411bbe134c1574081130ddbe50ffcec3543d
CBrandt <72421963+cbrandt83@users.noreply.github.com>
2020-12-18 16:11:01 -0800

    finished mod 3.5.5 - find winning candidate

--- 3.2.11_SkillDrill.py
+++ 3.2.11_SkillDrill.py
@@ -1,12 +1,16 @@
 #the list of dicts
 voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county": "Denver", "registered_voters":463353}, {"county":"Jefferson", "registered_voters":432438}]
 
-#loop through values in list of dicts
+#iterate through list of dictionaries
+# for dicts in voting_data:
+#     print(f"{dicts['county']} county has {dicts['registered_voters']:,} registered voters.")
+#get values 1 & 2 in list of dicts
 for value1 in voting_data:
     value_of_Key1 = value1['county']
+    print(value_of_Key1)
 for value2 in voting_data:
     value_of_Key2 = value2['registered_voters']
      
 #print message with the values of Key1 and Key1 for every dictionary
-message=(f"{value_of_Key1} county has {value_of_Key2:,} registered voters.")
-print(message)
+    message=(f"{value_of_Key1} county has {value_of_Key2:,} registered voters.")
+    print(message)
